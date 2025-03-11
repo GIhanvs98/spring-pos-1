ead66a0859ceaa97bac9fc328d9036ccfd905289
Gihanvs98 <darkcreationsl98@gmail.com>
2025-03-12 01:20:44 +0530

    configuration completed

--- src/main/java/com/eadp/pos/systen/api/CustomerController.java
+++ src/main/java/com/eadp/pos/systen/api/CustomerController.java
@@ -1,4 +1,16 @@
 package com.eadp.pos.systen.api;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+
+    @PostMapping
+    public String saveCustomer(){
+        return "save success";
+    }
+
 }
